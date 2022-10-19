8568c8c89a05c3920b9a6aee0d0c7b9bc837e899
NotPetya <86024311+notpetya1337@users.noreply.github.com>
2022-10-19 16:53:41 -0600

    localloader.py now works

--- localloader.py
+++ localloader.py
@@ -1,14 +1,63 @@
 import os
+import sys
+from configparser import ConfigParser
+import logging
+from tagging import Tagging
 
-rootdir = './'
-# TODO: load from config
+
+# logger
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logging.debug("logging started")
+logger = logging.getLogger(__name__)
 
 
+config = ConfigParser()
+config.read('config.ini')
+rootdir=config.get('storage','localimagedir')
+
+
+# list all subdirectories in a given folder
 def listdirs(folder):
-    for result in os.scandir(folder):
-        if result.is_dir():
-            print(result.path)
-            listdirs(result)
+    internallist = []
+    for root, directories, files in os.walk(folder):
+        for directory in directories:
+            internallist.append(os.path.join(root, directory))
+    return internallist
+
+# list all images in a given folder
+def listimages(subfolder):
+    imageextensions = (".png", ".jpg", ".gif", ".jpeg")
+    internallist = []
+    for file in os.listdir(subfolder):
+        if file.endswith(imageextensions):
+            imagepath = os.path.join(subfolder, file)
+            internallist.append(imagepath)
+    return internallist
 
 
-listdirs(rootdir)
+# open an image at a given path
+def get_image_content(image_path):
+    image = open(image_path, 'rb')
+    return image.read()
+
+# define folder and image lists globally
+allfolders = []
+imagelist = []
+
+tagging = Tagging(config)
+allfolders = listdirs(rootdir)
+def main():
+    while True:
+        tags = []
+        workingdir = allfolders.pop(0)
+        print(workingdir)
+        workingimages = listimages(workingdir)
+        for image in workingimages:
+            # TODO: make this open a file directly instead of copying the image
+            logger.info('opening image=%s', image)
+            image_content = get_image_content(image)
+            logger.info('getting tags for image=%s', image)
+            tags = tagging.get_tags(image_binary=image_content)
+            print(image, tags)
+
+main()
