5b7950d5ffa9f126e6c217e94732ecde2e791f1d
NotPetya <86024311+notpetya1337@users.noreply.github.com>
2022-10-20 09:23:09 -0600

    Clean up formatting

--- imagetagger.py
+++ imagetagger.py
@@ -1,5 +1,6 @@
 from pyexif import pyexif
 
+
 def read(path, tag):
     metadata = pyexif.ExifEditor(path)
     return metadata.getTag(tag)
@@ -8,6 +9,7 @@ def read(path, tag):
 def write(path, tag, data):
     metadata = pyexif.ExifEditor(path)
     metadata.setTag(tag, data)
+
 
 def update(path, tag, data):
     metadata = pyexif.ExifEditor(path)
