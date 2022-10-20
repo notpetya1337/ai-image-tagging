81f5b4eec64a87ac4e5a2cb6d282c6ac85481084
NotPetya <86024311+notpetya1337@users.noreply.github.com>
2022-10-20 10:00:44 -0600

    Add text OCR tagging to UserComment EXIF field

--- tagging.py
+++ tagging.py
@@ -9,12 +9,20 @@ class Tagging():
 
     def get_tags(self, image_binary):
         if self.tags_backend == 'google-vision':
-            tags = self.google_vision(image_binary=image_binary)
+            tags = self.google_vision_labels(image_binary=image_binary)
         elif self.tags_backend == 'aws-rekognition':
             tags = self.aws_rekognition(image_binary=image_binary)
         return tags
 
-    def google_vision(self, image_binary):
+
+    def get_text(self, image_binary):
+        if self.tags_backend == 'google-vision':
+            text = self.google_vision_light_ocr(image_binary=image_binary)
+        elif self.tags_backend == 'aws-rekognition':
+            text = self.aws_rekognition(image_binary=image_binary)
+        return text
+
+    def google_vision_labels(self, image_binary):
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.google_credentials
         os.environ["GOOGLE_CLOUD_PROJECT"] = self.google_project
         from google.cloud import vision
@@ -38,6 +46,22 @@ class Tagging():
         for label in labels:
             tags.append(label.description)
         return tags
+
+    def google_vision_light_ocr(self, image_binary):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.google_credentials
+        os.environ["GOOGLE_CLOUD_PROJECT"] = self.google_project
+        from google.cloud import vision
+        client = vision.ImageAnnotatorClient()
+        # Loads the image into memory
+        image = vision.Image(content=image_binary)
+        # image = client.annotate_image({'content': image_binary})
+        # Performs label detection on the image file
+        responsetags = client.text_detection(image=image)
+        textobject = responsetags.text_annotations
+        returntext = []
+        for text in textobject:
+            returntext.append(text.description)
+        return returntext
 
     def aws_rekognition(self, image_binary):
         return True
