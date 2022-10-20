66b44e68bf8b71562b2ddaaee7d3500decd3679f
NotPetya <86024311+notpetya1337@users.noreply.github.com>
2022-10-20 09:11:05 -0600

    Write tags returned by Vision AI to EXIF "ImageDescription" field

---
+++ imagetagger.py
@@ -0,0 +1,16 @@
+from pyexif import pyexif
+
+def read(path, tag):
+    metadata = pyexif.ExifEditor(path)
+    return metadata.getTag(tag)
+
+
+def write(path, tag, data):
+    metadata = pyexif.ExifEditor(path)
+    metadata.setTag(tag, data)
+
+def update(path, tag, data):
+    metadata = pyexif.ExifEditor(path)
+    internal = metadata.getTags(tag)
+    internal.append(data)
+    metadata.setTag(tag, internal)
